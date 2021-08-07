4c2732c2094733f03c700c9afd0f92597e1c028b
Rommy <arbantas@gmail.com>
2021-08-07 09:12:49 +0800

    update KartuMenu

--- components/KartuMenu.tsx
+++ components/KartuMenu.tsx
@@ -1,26 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
-import { useWindowWidth } from '@react-hook/window-size'
 //
 import { KartuMenuProps } from '../types/componentProps'
 import appColors from '../constants/appColors'
 
-const KartuMenu: React.FC<KartuMenuProps> = ({ ikon, judul, deskipsi }) => {
-  const windowWidth = useWindowWidth()
-  const cardSize = React.useMemo<number>(() => {
-    if (windowWidth < 375) {
-      return 150
-    } else if (windowWidth < 640) {
-      return 170
-    }
-    return 175
-  }, [windowWidth])
+const cardSize = 175
 
+const KartuMenu: React.FC<KartuMenuProps> = ({ ikon, judul, deskipsi }) => {
   return (
     <div
       className="shadow border-t rounded-lg p-4"
       style={{
-        height: windowWidth >= 375 ? cardSize : 'auto',
+        height: cardSize,
         width: cardSize,
       }}
     >
